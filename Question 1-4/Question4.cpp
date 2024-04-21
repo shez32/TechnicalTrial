c6d056de0ae65fb5a7c45c55025b77f13fabe024
shez32 <42356271+shez32@users.noreply.github.com>
2024-04-22 03:30:51 +0500

    Updated Question4.cpp
    fixed comments and indentation errors

--- Question 1-4/Question4.cpp
+++ Question 1-4/Question4.cpp
@@ -8,13 +8,13 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 	
 	// If the player doesn't exist, create a new one and attempt to load data
 	if (!player) {
-		player = new Player(nullptr);	// Create a new player
-		createdNewPlayer = true;		// Set the flag to indicate we created a new player
+		player = new Player(nullptr);			// Create a new player
+		createdNewPlayer = true;			// Set the flag to indicate we created a new player
 		
 		// Try to load player data; if it fails, delete the player to avoid memory leak
 		if (!IOLoginData::loadPlayerByName(player, recipient)) {
 			delete player;  			// Proper cleanup in case of failure
-			return;						// Exit if player loading fails
+			return;					// Exit if player loading fails
 		}
 	}
 
@@ -24,10 +24,9 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 	 // If item creation fails, clean up newly created player, if any
 	if (!item) {
 		if (createdNewPlayer) {
-            delete player;  			// Cleanup if the item creation failed and we created a new player
-        }
-		
-		return;							// Exit if item creation fails
+            		delete player;  			// Cleanup if the item creation failed and we created a new player
+        	}	
+		return;						// Exit if item creation fails
 	}
 
 	// Add the item to the player's inbox
@@ -36,13 +35,12 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 	// If the player is offline, save their data
 	if (player->isOffline()) {
 		IOLoginData::savePlayer(player);
-		
 		// If we created a new player, clean up after saving
 		if (createdNewPlayer) {
-            delete player;  			// Avoid memory leak
-        }
-    }   
+            		delete player;  			// Avoid memory leak
+        	}
+    	}   
 	else if (createdNewPlayer) {
-        delete player;  				// If we created a new player but they're online, clean up after adding the item
-    }
+        	delete player;  				// If we created a new player but they're online, clean up after adding the item
+    	}
 }
